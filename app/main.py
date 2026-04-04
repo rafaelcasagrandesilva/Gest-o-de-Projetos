57479c29daffe5521baf738abe3d161e02c7bbfe
Rafael Casagrande <rafaelcasagrande@MacBook-Air-de-Rafael.local>
2026-04-04 19:32:18 -0300

    liberar CORS temporariamente

--- app/main.py
+++ app/main.py
@@ -17,11 +17,7 @@ logger = logging.getLogger(__name__)
 app = FastAPI(title=settings.app_name)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://artistic-connection-production-21ce.up.railway.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
